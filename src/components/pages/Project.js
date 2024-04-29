6abc173992b3009e7a74ce5526db257d80c1cdcc
Dimas Alves Pereira <dimasdevspro@gmail.com>
2024-04-29 16:17:19 -0300

    created services area

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,6 +13,7 @@ function Project() {
     const {id} = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -34,6 +35,10 @@ function Project() {
     
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
     }
 
     function editPost(project){
@@ -95,6 +100,19 @@ function Project() {
                             </div>
                         )}
                     </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && <div>formulário do serviço</div>}
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de serviços</p>
+                        </Container>
                 </Container>
             </div>
         ):( 
